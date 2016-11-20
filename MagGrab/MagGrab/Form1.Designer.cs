5e99fe1060089ac11f4f84ce6af510806f41d7e0
毛才明 <380957580@qq.com>
2016-11-20 15:40:33 +0800

    begin

---
+++ MagGrab/MagGrab/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace MagGrab
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbUrl = new System.Windows.Forms.TextBox();
+            this.btnGet = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "地址：";
+            // 
+            // tbUrl
+            // 
+            this.tbUrl.Location = new System.Drawing.Point(60, 10);
+            this.tbUrl.Name = "tbUrl";
+            this.tbUrl.Size = new System.Drawing.Size(311, 21);
+            this.tbUrl.TabIndex = 1;
+            this.tbUrl.Text = "http://cili15.com/";
+            // 
+            // btnGet
+            // 
+            this.btnGet.Location = new System.Drawing.Point(377, 8);
+            this.btnGet.Name = "btnGet";
+            this.btnGet.Size = new System.Drawing.Size(75, 23);
+            this.btnGet.TabIndex = 2;
+            this.btnGet.Text = "获取";
+            this.btnGet.UseVisualStyleBackColor = true;
+            this.btnGet.Click += new System.EventHandler(this.btnGet_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser1.Location = new System.Drawing.Point(13, 41);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(439, 353);
+            this.webBrowser1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(462, 406);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.btnGet);
+            this.Controls.Add(this.tbUrl);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "磁力抓取";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbUrl;
+        private System.Windows.Forms.Button btnGet;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ MagGrab/MagGrab/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MagGrab
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGet_Click(object sender, EventArgs e)
+        {
+            //HttpClient client = new HttpClient();
+            //var result = client.GetStringAsync(tbUrl.Text);
+            //var content = result.Result;
+            webBrowser1.Url =new Uri( tbUrl.Text);
+            webBrowser1.DocumentCompleted += WebBrowser1_DocumentCompleted;
+        }
+
+        private void WebBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            HtmlDocument document = webBrowser1.Document;
+            var dl = document.GetElementsByTagName("dl");
+            var dds = document.GetElementsByTagName("dd");
+            Regex reg = new Regex(@"^(.*E\d{2}.*)|(.*Ep\d{2}.*)$");
+            foreach (HtmlElement dd in dds)
+            {
+                if(reg.IsMatch(dd.InnerText))
+                {
+                    dd.OuterHtml = null;
+                }
+            }
+        }
+    }
+}
